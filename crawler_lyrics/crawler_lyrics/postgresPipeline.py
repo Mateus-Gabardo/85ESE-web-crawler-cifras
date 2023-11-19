d747d4f09dd10b8ab4e2fcd64158c858bab96e92
Mateus Gabardo Lemos <gabardomateus@gmail.com>
2023-11-19 20:12:20 -0300

    alteracoes

--- crawler_lyrics/crawler_lyrics/postgresPipeline.py
+++ crawler_lyrics/crawler_lyrics/postgresPipeline.py
@@ -1,28 +1,20 @@
-import datetime
-from itemadapter import ItemAdapter
+
 import psycopg2
+from itemadapter import ItemAdapter
 
 
-class CrawlerLyricsPipeline:
-    def __init__(self, database):
-        self.database = database
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        settings = crawler.settings
-        print(settings)
-        database = {
-            'host': settings.get('DATABASE')['host'],
-            'user': settings.get('DATABASE')['user'],
-            #'password': settings.get('DATABASE')['password'],
-            'database': settings.get('DATABASE')['database'],
-            'port': settings.get('DATABASE')['port'],
-        }
-        return cls(database)
-    
+class PostgresPipeline:
     def open_spider(self, spider):
-        self.connection = psycopg2.connect(**self.database)
+        hostname = 'localhost'
+        username = 'admin'
+        password = '1234'
+        database = 'postgres'
+        self.connection = psycopg2.connect(
+            host=hostname, user=username, password=password, 
+            dbname=database)
+        
         self.cur = self.connection.cursor()
+
         self.cur.execute("""
             CREATE TABLE IF NOT EXISTS catholic_lyrics(
                 id serial PRIMARY KEY,
@@ -33,13 +25,11 @@ class CrawlerLyricsPipeline:
                 link_ref VARCHAR(250)
             );
             """)
+
     def close_spider(self, spider):
-        finish_time = datetime.now()
-        elapsed_time = finish_time - self.start_time
-        spider.log(f"Spider ran for: {elapsed_time}")
         self.cur.close()
         self.connection.close()
-    
+
     def process_item(self, item, spider):
         try:
             self.cur.execute(
--- crawler_lyrics/crawler_lyrics/spiders/CifrasClubSpider.py
+++ crawler_lyrics/crawler_lyrics/spiders/CifrasClubSpider.py
@@ -1,28 +1,67 @@
-# -*- coding: utf-8 -*-
+
 import scrapy
 import re
 
+from crawler_lyrics.items import CrawlerLyricsItem
+
 
 class CifrasClubSpider(scrapy.Spider):
-    name = 'Cifras-Club'
+    name = 'cifras_club'
     allowed_domains = ['cifraclub.com.br']
     start_urls = ['https://www.cifraclub.com.br/catolicas/']
 
     def parse(self, response):
-        music_elements = response.xpath('//li[@data-name]')
-        domain_url = 'https://www.cifraclub.com.br'
+        song_list  = response.css('ul#js-a-songs li')
 
-        for music_element in music_elements:
-            music_link = domain_url + music_element.css('a.art_music-link::attr(href)').get()
-
-            yield response.follow(music_link, self.parse_lyrics)
+        for song in song_list :
+            song_link = song.css('a.art_music-link::attr(href)').get()
+            yield response.follow(song_link, self.parse_lyrics)
 
     def parse_lyrics(self, response):
         titulo = response.css('h1.t1::text').get()
-        compositor = response.css('div.cifra-footer a::text').get().replace('Composição de ', '') 
+        compositor = response.css('div.cifra-footer a::text').get().replace('Composição de ', '')
         link = response.url
+        cifras = self.trataCifras(response.css('pre').get())
+        lyrics = self.trataLyrics(response.css('pre').get())
 
-    def removeTag(rawHtml):
-        regex = re.compile('<.*?>')
-        return re.sub(regex, '', rawHtml)
+        item = CrawlerLyricsItem(title=titulo, lyric=lyrics, author=compositor, cifra=cifras, link_ref=link)
+        yield item
+    
+    def trataCifras(self, rawHtml):
+        return self.remove_specific_tag(rawHtml, '<pre>')
+    
+    def trataLyrics(self, rawHtml):
+        leters = self.remove_all_between_tag(rawHtml, 'b')
+        leters = self.remove_all_tags(leters)
+        return leters
+
+    def remove_all_between_tag(self, raw_html, tag):
+        regex = re.compile(rf'<{tag}[^>]*>(.+?)</{tag}>', re.DOTALL)
+        if raw_html is not None:
+            return re.sub(regex, '', raw_html)
+        else:
+            return raw_html
+    
+    def remove_specific_tag(self, rawHtml, tag):
+        if rawHtml is not None and tag is not None:
+            regex = re.compile(rf'<{tag}[^>]*>|<\/{tag}>', re.DOTALL)
+            rawHtml = re.sub(regex, '', rawHtml)
+        return rawHtml
+
+    
+    def remove_all_tags(self, raw_html):
+        if raw_html is not None:
+            without_tags = re.sub(r'<.*?>', '', raw_html)
+            without_newlines = self.remove_empty_lines(without_tags)
+            return without_newlines
+        else:
+            return raw_html
+        
+    def remove_empty_lines(self, text):
+        lines = text.split('\n')
+        non_empty_lines = [line for line in lines if line.strip() != '']
+        result = '\n'.join(non_empty_lines)
+        return result
+
+        
         
