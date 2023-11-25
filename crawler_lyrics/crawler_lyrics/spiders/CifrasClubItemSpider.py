8c1155c9290e49f0d384073edb200ccde91e43ee
Mateus Gabardo Lemos <gabardomateus@gmail.com>
2023-11-25 19:47:57 -0300

    Creação do spider com retorno de um unico item

---
+++ crawler_lyrics/crawler_lyrics/spiders/CifrasClubItemSpider.py
@@ -0,0 +1,54 @@
+import scrapy
+import re
+
+from crawler_lyrics.items import CrawlerLyricsItem
+
+class CifrasClubItemSpider(scrapy.Spider):
+    name = 'cifras_club_item'
+    allowed_domains = ['cifraclub.com.br']
+    start_urls = ['https://www.cifraclub.com.br/catolicas/sacramento-da-comunhao/']
+
+    def parse(self, response):
+        titulo = response.css('h1.t1::text').get()
+        compositor = response.css('div.cifra-footer a::text').get().replace('Composição de ', '')
+        link = response.url
+        cifras = self.trataCifras(response.css('pre').get())
+        lyrics = self.trataLyrics(response.css('pre').get())
+
+        item = CrawlerLyricsItem(title=titulo, lyric=lyrics, author=compositor, cifra=cifras, link_ref=link)
+        yield item
+    
+    def trataCifras(self, rawHtml):
+        return self.remove_specific_tag(rawHtml, '<pre>')
+    
+    def trataLyrics(self, rawHtml):
+        leters = self.remove_all_between_tag(rawHtml, 'b')
+        leters = self.remove_all_tags(leters)
+        return leters
+
+    def remove_all_between_tag(self, raw_html, tag):
+        regex = re.compile(rf'<{tag}[^>]*>(.+?)</{tag}>', re.DOTALL)
+        if raw_html is not None:
+            return re.sub(regex, '', raw_html)
+        else:
+            return raw_html
+    
+    def remove_specific_tag(self, rawHtml, tag):
+        if rawHtml is not None and tag is not None:
+            regex = re.compile(rf'<{tag}[^>]*>|<\/{tag}>', re.DOTALL)
+            rawHtml = re.sub(regex, '', rawHtml)
+        return rawHtml
+
+    def remove_all_tags(self, raw_html):
+        if raw_html is not None:
+            without_tags = re.sub(r'<.*?>', '', raw_html)
+            without_newlines = self.remove_empty_lines(without_tags)
+            return without_newlines
+        else:
+            return raw_html
+        
+    def remove_empty_lines(self, text):
+        lines = text.split('\n')
+        non_empty_lines = [line for line in lines if line.strip() != '']
+        result = '\n'.join(non_empty_lines)
+        return result
